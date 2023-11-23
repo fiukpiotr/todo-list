6d7f3923eb06057f12e3c86ebbb18e0ece6bc43c
Pete Fiuk <piotrek.fiuk@gmail.com>
2023-11-23 12:10:49 +0100

    Updates

---
+++ precache-manifest.7d22e41debc1839a43a31e22c5a5934b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cd7c7a3fde2c6d8f41c12df75c07fc",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2ef093628ee37d437ae2",
+    "url": "/todo-list/static/css/main.f23da7ad.chunk.css"
+  },
+  {
+    "revision": "e60f9264537e03f019fe",
+    "url": "/todo-list/static/js/2.366ff559.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/todo-list/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "2ef093628ee37d437ae2",
+    "url": "/todo-list/static/js/main.7ac5edc2.chunk.js"
+  },
+  {
+    "revision": "b0f28bf0bc35679affb6",
+    "url": "/todo-list/static/js/runtime~main.0c9b2218.js"
+  },
+  {
+    "revision": "d34d4a7114d86e5ef67b305b6b92aab2",
+    "url": "/todo-list/static/media/3px-tile.d34d4a71.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06c591a9b0695735ceb273a02950c298.js"
+  "/todo-list/precache-manifest.7d22e41debc1839a43a31e22c5a5934b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
